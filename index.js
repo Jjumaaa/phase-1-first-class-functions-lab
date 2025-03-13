64a8c4562e1028ebd4bdedb21a659dadfbf642d0
Juma <gerald.juma@student.moringaschool.com>
2025-03-13 15:54:16 +0300

    change

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, whichDrivers) {
-  return whichDrivers(drivers);
+const selectDifferentDrivers = function(drivers, thoseDeres) {
+  return thoseDeres(drivers);
 }
